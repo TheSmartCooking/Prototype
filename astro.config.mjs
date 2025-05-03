8be7d8628ff8634499d0113218d6190be8d5884b
Vianpyro <vianney@veremme.org>
2025-05-03 13:01:37 +0000

    Update Astro configuration and package dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,12 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'static',
     build: {
         format: 'file',
         assets: 'compressed',
+        assetsPrefix: './',
     },
+    output: 'static',
     vite: {
         server: {
             watch: {
