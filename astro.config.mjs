6af62a7e919aba102ec2c425cd9c6d51d897060c
Vianney Veremme <10519369+Vianpyro@users.noreply.github.com>
2025-05-04 17:48:13 -0400

    Reproduce actual website (#2)
    * Update Astro configuration and package dependencies
    
    * Add SearchBar component and integrate it into the Header; update styles and media queries
    
    * Update background gradients in SVG and adjust color variables in global CSS for improved theming
    
    * Refactor typography and SVG dimensions: update h1 font size and svg dimensions to use medium spacing for consistency
    
    * Refactor Header component: improve structure and styling for better responsiveness and consistency
    
    * Enhance Header component: implement scroll-based visibility toggle and refine layout structure in Main layout
    
    * Update SearchBar placeholder text for clarity
    
    * Refactor SearchBar styles: reorganize CSS for improved readability and maintainability
    
    * Refactor SearchBar layout: change flex property to width for better responsiveness
    Enhance Main layout: add margin-top for main section on small screens
    Update global styles: adjust polaroid photo size and width for improved layout on small devices
    
    * Add conditional rendering for SearchBar in Header component
    
    * Add authentication page with login and registration forms, including eye icons for password visibility
    
    * Update authentication page: enhance password input with visibility toggle icons and improve layout
    
    * Add vite-svg-loader to devDependencies in package.json and package-lock.json
    
    * Add vite-svg-loader plugin to Astro configuration for SVG support
    
    * Refactor icon usage in components: replace Icon component with individual SVG imports for GitHub, Contact, Privacy, Search, and Password visibility icons
    
    * Enhance password visibility toggle: add title attribute for accessibility and style adjustments

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
+import svgLoader from 'vite-svg-loader'
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'static',
     build: {
         format: 'file',
         assets: 'compressed',
+        assetsPrefix: './',
     },
+    output: 'static',
     vite: {
+        plugins: [svgLoader()],
         server: {
             watch: {
                 usePolling: true,
